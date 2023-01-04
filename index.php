bcd92c84d78d6b07da7deb6015755e4121666962
nadir inab <nadir.inab.dev@gmail.com>
2023-01-04 15:39:11 +0100

    code has been added

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php 
-/*
-    session_start()
-*/
-// require_once './_config/config.php';
+
+    require_once "./vendor/autoload.php" ;
+
+    use Egulias\EmailValidator\EmailValidator;
+    use Egulias\EmailValidator\Validation\RFCValidation;
+
+    $email = "nadir.inab.dev@gmail.com" ;
+    $validator = new EmailValidator() ;
+    $validator->isValid($email, new RFCValidation()) ;
+   
+
 
