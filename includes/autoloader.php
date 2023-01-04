43fc789e09fa65f4646f42bcc53c5dbe0c791011
nadir inab <nadir.inab.dev@gmail.com>
2023-01-04 11:40:57 +0100

    autoloading function is well commented

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -1,9 +1,17 @@
 <?php
-/* 
-    instead of including multiple files on each page
-    spl_autoload_register will include appropriate 
-    path of your class sequentially when an 
-    object is is instantiated(new Class). 
+
+/*
+|--------------------------------------------------------------------------
+|                          spl_autoload_register                          |
+|--------------------------------------------------------------------------
+|                Q-Why use autoloading ?
+|    Answer : We want to be able to load the future classes without any pain (see: dozen of include or require) .
+|
+|   instead of including multiple files on each page
+|   spl_autoload_register will include appropriate 
+|   path of your class sequentially when an 
+|   object is is instantiated(new Class). 
 */
+
 spl_autoload_register() ;
 
