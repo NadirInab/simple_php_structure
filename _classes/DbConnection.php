0ba05dfcb15ddeaf8d084acd0043100a8c9867f0
nadir inab <nadir.inab.dev@gmail.com>
2023-01-04 11:38:55 +0100

    connection class is added successfully

--- _classes/DbConnection.php
+++ _classes/DbConnection.php
@@ -1,5 +1,18 @@
 <?php 
 
-/*  your connection class goes here 
-    hint : use PDO . 
-*/
+class DbConnection {
+    private $host = "" ; // your Host
+    private $user = "" ; // your user Name 
+    private $pwd = "" ; // your pwd 
+    private $dbName = "" ; // your DataBase Name .
+
+    public function connect(){
+        try{
+            $dsn = "mysql:host=$this->host; dbname=$this->dbName" ;
+            $pdo = new PDO($dsn, $this->user, $this->pwd,[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]) ;
+            return $pdo ;
+        }catch(PDOException $e){
+            echo "connection is failed ".$e->getMessage() ;
+        }
+    }
+}
